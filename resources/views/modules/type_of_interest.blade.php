aa79b9dc5569c8c3207753452b1b78992b544afd
Ascaliko <ascaliko7@gmail.com>
2023-12-17 03:03:09 +0700

    update module toi

--- resources/views/modules/type_of_interest.blade.php
+++ resources/views/modules/type_of_interest.blade.php
@@ -27,7 +27,7 @@
 										<ul class="breadcrumb breadcrumb-separatorless fw-semibold mb-6">
 											<!--begin::Item-->
 											<li class="breadcrumb-item text-gray-700 fw-bold lh-1">
-												<a href="../dist/index.html" class="text-gray-500">
+												<a href="javascript:void(0)" class="text-gray-500">
 													<i class="ki-duotone ki-home fs-3 text-gray-400 me-n1"></i>
 												</a>
 											</li>
@@ -56,7 +56,7 @@
 									</div>
 									<!--end::Page title-->
 									<!--begin::Actions-->
-									<a href="javascript:void(0)" class="btn btn-sm btn-success ms-3 px-4 py-3 sipayu_modal">Create</a>
+									<a href="javascript:void(0)" class="btn btn-sm btn-success ms-3 px-4 py-3 sipayu_modal d-none">Tambah Data</a>
 									<!--end::Actions-->
 								</div>
 								<!--end::Toolbar wrapper-->
@@ -78,53 +78,18 @@
 											<div class="card-body pt-6">
                                                 
                                                 <div class="table-responsive">
-                                                    <table id="kt_datatable_zero_configuration" class="table table-row-bordered gy-5">
+                                                    <table id="datatable_sipayu" class="table table-row-bordered gy-5">
                                                         <thead>
                                                             <tr class="fw-semibold fs-6 text-muted">
-                                                                <th>Name</th>
-                                                                <th>Position</th>
-                                                                <th>Office</th>
-                                                                <th>Age</th>
-                                                                <th>Start date</th>
-                                                                <th>Salary</th>
+                                                                <th>name</th>
+                                                                <th>image</th>
+                                                                <th>description</th>
+																<th>Dibuat</th>
+																<th></th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
-                                                            <tr>
-                                                                <td>Tiger Nixon</td>
-                                                                <td>System Architect</td>
-                                                                <td>Edinburgh</td>
-                                                                <td>61</td>
-                                                                <td>2011/04/25</td>
-                                                                <td>$320,800</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>Garrett Winters</td>
-                                                                <td>Accountant</td>
-                                                                <td>Tokyo</td>
-                                                                <td>63</td>
-                                                                <td>2011/07/25</td>
-                                                                <td>$170,750</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>Ashton Cox</td>
-                                                                <td>Junior Technical Author</td>
-                                                                <td>San Francisco</td>
-                                                                <td>66</td>
-                                                                <td>2009/01/12</td>
-                                                                <td>$86,000</td>
-                                                            </tr>
                                                         </tbody>
-                                                        <tfoot>
-                                                            <tr>
-                                                                <th>Name</th>
-                                                                <th>Position</th>
-                                                                <th>Office</th>
-                                                                <th>Age</th>
-                                                                <th>Start date</th>
-                                                                <th>Salary</th>
-                                                            </tr>
-                                                        </tfoot>
                                                     </table>
                                                 </div>
 
@@ -153,6 +118,336 @@
 		<!--end::Page-->
 	</div>
 	<!--end::App-->
+
+	<!-- Modal -->
+	<form id="form">
+		<div class="modal fade" id="modalForm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="modalFormLabel" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-scrollable">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="modalFormLabel">Form {{ $title }}</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+						<input type="hidden" name="id" id="id" />
+						<div class="mb-3">
+							<label for="name" class="col-form-label">Nama:</label>
+							<input type="text" name="name" class="form-control" id="name" required />
+						</div>
+						<div class="mb-3">
+							<label for="description" class="col-form-label">Description:</label>
+							<textarea class="form-control" name="description" id="description"></textarea>
+						</div>
+						<div class="mb-3">
+						  <label for="image" class="form-label">Gambar Iklan</label>
+						  <input class="form-control form-control-sm" id="file" accept=".jpg, .png, .jpeg" type="file" onchange="preview_image()" />
+						  <input type="hidden" name="image" id="imageBase64" />
+						</div>
+						<div class="mb-3"><img id="ads-img-thumbnail" src="" class="img-thumbnail mx-auto d-block" alt="..."></div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+					<button type="sumit" class="btn btn-primary">Submit</button>
+				</div>
+				</div>
+			</div>
+		</div>
+	</form>
+
+    <script>
+		var isModuleApi = `ads`;
+		var action;
+        $(document).ready(function() {
+			oTable = $('#datatable_sipayu').DataTable( {
+				'ajax': {
+					'url': `${baseUrlApi}/api/type_of_interest/list_dt`,
+					'type': 'GET',
+					'beforeSend': function (request) {
+						request.setRequestHeader("Authorization", `Bearer ${apiKey}`);
+					}
+				},
+				"processing":true,
+				"serverSide":true,
+				"stateSave":true,
+				"bDestroy": true,
+				"retrieve": true,
+				"columns": [
+					{ "data": "name" },
+					{ "data": null, "render": (row) => `<img src="${row.image}" class="rounded mx-auto d-block" alt="" style="width: 100px;" />` },
+					{ "data": "description"  },
+					{ "data": "created_at" },
+					{ 
+						"data": null,
+						"render": function(row) {
+							let html = `
+								<a href="javascript:void(0)" class="btn btn-sm btn-info edit" data-id="${row.id}">Ubah</a>
+								<a href="javascript:void(0)" class="btn btn-sm btn-danger delete d-none" data-id="${row.id}">Hapus</a>
+							`;
+							return html;
+						}
+					},
+				]
+			} );
+        });
+		$(document).on("keyup", ".search-input", function() {
+			oTable.search($(this).val()).draw() ;
+		});
+		$(document).on("click", ".sipayu_modal", function() {
+			action = "add";
+			$("#ads-img-thumbnail").attr("src", "");
+			$("#modalForm").modal("show");
+			
+		});
+		$(document).on("click", ".edit", function() {
+			action = "edit";
+			let id = $(this).attr("data-id");
+			$("#id").val(id);
+			getData(id);
+			$("#modalForm").modal("show");
+		});
+		$(document).on("click", ".delete", async function() {
+			let id = $(this).attr("data-id");
+			$("#id").val(id);
+			await deleteData(id);
+		});
+		$(document).on("submit", "#form", async function(e) {
+			e.preventDefault();
+			await submitData();
+		});
+
+		async function getData(id) {
+			let config = {
+			  method: 'get',
+			  maxBodyLength: Infinity,
+			  url: `${baseUrlApi}/api/type_of_interest/${id}`,
+			  headers: { 
+				"Authorization": `Bearer ${apiKey}`
+			  }
+			};
+			
+			await axios.request(config)
+			.then((response) => {
+			  console.log(JSON.stringify(response.data));
+			  let data = response.data.data;
+			  $("#name").val(data.name);
+			  $("#imageBase64").val(data.image);
+			  $("#ads-img-thumbnail").attr("src", data.image);
+			  $("#description").val(data.description);
+			})
+			.catch((error) => {
+			  console.log(error);
+			  swalFailed();
+			});
+
+		}
+
+		async function deleteData(id) {
+			swalWithBootstrapButtons.fire({
+				title: "Apakah yakin?",
+				text: "Tidak akan dapat dikembalikan!",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonText: "Ya, hapus!",
+				cancelButtonText: "Tidak, batal!",
+				reverseButtons: true
+			}).then(async (result) => {
+				if (result.isConfirmed) {
+					await execute(id);
+					await swalWithBootstrapButtons.fire({
+						title: "Terhapus!",
+						text: "Data Anda telah dihapus.",
+						icon: "success"
+					});
+					setTimeout(async () => {
+						await oTable.ajax.reload( null, false );
+					}, 1000);
+				} else if (
+					result.dismiss === Swal.DismissReason.cancel
+				) {
+					await swalWithBootstrapButtons.fire({
+						title: "Dibatalkan",
+						text: "Data Anda aman!",
+						icon: "error"
+					});
+				}
+			});
+
+			let execute = async (id) => {
+				let config = {
+				  method: 'delete',
+				  maxBodyLength: Infinity,
+				  url: `${baseUrlApi}/api/type_of_interest/delete/${id}`,
+				  headers: { 
+					"Authorization": `Bearer ${apiKey}` 
+				  }
+				};
+				
+				await axios.request(config)
+				.then((response) => {
+				  console.log(JSON.stringify(response.data));
+					Swal.fire({
+						text: "Berhasil!",
+						icon: "success",
+						buttonsStyling: false,
+						confirmButtonText: "Ok, mengerti!",
+						customClass: {
+							confirmButton: "btn btn-primary"
+						}
+					});
+				})
+				.catch((error) => {
+				  console.log(error);
+				  	swalFailed();
+				});
+			}
+				
+
+		}
+
+		async function submitData() {
+			if ($("#imageBase64").val()!='') {
+				let data = {
+				  "name": $("#name").val(),
+				  "image": $("#imageBase64").val(),
+				  "description": $("#description").val(),
+				};
+				data = JSON.stringify(data);
+				let isUrl = action == "add" ? `${baseUrlApi}/api/type_of_interest/create` : `${baseUrlApi}/api/type_of_interest/update/${$("#id").val()}`;
+				let config = {
+				  method: 'post',
+				  maxBodyLength: Infinity,
+				  url: isUrl,
+				  headers: { 
+					'Content-Type': 'application/json', 
+					"Authorization": `Bearer ${apiKey}`
+				  },
+				  data : data
+				};
+				
+				await axios.request(config)
+				.then(async (response) => {
+				  console.log(JSON.stringify(response.data));
+					await swalWithBootstrapButtons.fire({
+						title: "Berhasil!",
+						text: "Data Anda telah disubmit.",
+						icon: "success"
+					});
+					setTimeout(async () => {
+						await oTable.ajax.reload( null, false );
+					}, 1000);
+					setTimeout(async () => {
+						await $('form').trigger("reset");
+					}, 1000);
+					$("#modalForm").modal("hide");
+				})
+				.catch((error) => {
+				  console.log(error);
+					swalFailed();
+				});
+			} else {
+				await swalWithBootstrapButtons.fire({
+					title: "Galat!",
+					text: "File Gambar harus di isi!",
+					icon: "error"
+				});
+			}
+		}
+
+		function readFile() {
+			if (!this.files || !this.files[0]) return;
+			const FR = new FileReader();
+			FR.addEventListener("load", function(evt) {
+				document.querySelector("#ads-img-thumbnail").src = evt.target.result;
+				document.querySelector("#imageBase64").value	 = evt.target.result;
+			}); 
+			FR.readAsDataURL(this.files[0]);
+		}
+		// document.querySelector("#file").addEventListener("change", readFile);
+
+		async function preview_image() {
+			const file = document.getElementById('file');
+			const image = await process_image(file.files[0]);
+			document.querySelector("#ads-img-thumbnail").src = image;
+			document.querySelector("#imageBase64").value	 = image;
+			// console.log(image)
+		}
+
+		async function reduce_image_file_size(base64Str, MAX_WIDTH = 450, MAX_HEIGHT = 450) {
+			let resized_base64 = await new Promise((resolve) => {
+				let img = new Image()
+				img.src = base64Str
+				img.onload = () => {
+					let canvas = document.createElement('canvas')
+					let width = img.width
+					let height = img.height
+		
+					if (width > height) {
+						if (width > MAX_WIDTH) {
+							height *= MAX_WIDTH / width
+							width = MAX_WIDTH
+						}
+					} else {
+						if (height > MAX_HEIGHT) {
+							width *= MAX_HEIGHT / height
+							height = MAX_HEIGHT
+						}
+					}
+					canvas.width = width
+					canvas.height = height
+					let ctx = canvas.getContext('2d')
+					ctx.drawImage(img, 0, 0, width, height)
+					resolve(canvas.toDataURL()) // this will return base64 image results after resize
+				}
+			});
+			return resized_base64;
+		}
+		
+		async function image_to_base64(file) {
+			let result_base64 = await new Promise((resolve) => {
+				let fileReader = new FileReader();
+				fileReader.onload = (e) => resolve(fileReader.result);
+				fileReader.onerror = (error) => {
+					console.log(error)
+					alert('An Error occurred please try again, File might be corrupt');
+				};
+				fileReader.readAsDataURL(file);
+			});
+			return result_base64;
+		}
+		
+		async function process_image(file, min_image_size = 300) {
+			const res = await image_to_base64(file);
+			let dataBase64 = "";
+			if (res) {
+				const old_size = calc_image_size(res);
+				if (old_size > min_image_size) {
+					const resized = await reduce_image_file_size(res);
+					const new_size = calc_image_size(resized)
+					console.log('new_size=> ', new_size, 'KB');
+					console.log('old_size=> ', old_size, 'KB');
+					dataBase64 = resized;
+				} else {
+					console.log('image already small enough')
+					dataBase64 = res;
+				}
+		
+			} else {
+				console.log('return err')
+				dataBase64 = '';
+			}
+			return dataBase64;
+		}
+		
+		function calc_image_size(image) {
+			let y = 1;
+			if (image.endsWith('==')) {
+				y = 2
+			}
+			const x_size = (image.length * (3 / 4)) - y
+			return Math.round(x_size / 1024)
+		}
+    </script>
+
 	@include('layouts.scrolltop')
 	@include('layouts.modal_template')
 @include('layouts.footer')
